9f58c759aa434e43490d912c49cfcc8e2d82161c
Brunacamp <camposcampelobruna@gmail.com>
2025-01-29 08:18:58 -0300

    Initial commit

---
+++ ATIVIDADES/Lista1/aritimeticos.js
@@ -0,0 +1,43 @@
+//1
+let a = 5
+let b = 2
+
+let soma = a + b
+let sub = a - b
+let mult = a * b
+let div = a / b
+let resto = a % b
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(resto)
+
+//2
+let largura = 100
+let altura = 300
+
+let mult2 = largura * altura
+
+console.log(mult)
+
+//3
+let num = 10
+
+let dobro = 2 * num
+let metade = num / 2
+console.log(dobro)
+console.log(metade)
+
+//4
+let num1 = 5
+let num2 = 10
+let num3 = 7
+
+let media = (num1 + num2 + num3) /3
+console.log(media)
+
+//5
+let cel = 10
+let fah = (cel * 9/5) +32
+console.log(fah)
---
+++ operadores/aritimeticos.js
@@ -0,0 +1,28 @@
+// console.log("olá mundo!")
+
+let nome = "bruna" // vamos preferir esse
+var nomes = "juliana"
+
+// CTRL + ; -> COMENTARIOS
+console.log(nome)
+
+let idade = 24
+console.log("minha idade é ", idade, "anos")
+// CONCATENAÇÃO - juntar texto e valor
+
+// OPERAÇÕES MATEMATICAS
+
+let a = 3
+let b = 2
+
+let soma = a + b
+let sub = a - b
+let mult = a * b
+let div = a / b
+let resto = a % b //  o que sobra da divisão
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(resto)
